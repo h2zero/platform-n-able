97b0b91f8b628c69bb706cb4bc15fa6953e9b8be
h2zero <powellperalta@gmail.com>
2021-12-31 13:30:26 -0700

    add basic example/remove others

---
+++ examples/arduino-nrf51-test/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "NimBLEDevice.h"
+
+void setup() {
+  NimBLEDevice::init("");
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
